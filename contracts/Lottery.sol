e07578a7e11879455d0b096f4ce046f1c484b8f6
ppoply <34216657+ppoply@users.noreply.github.com>
2018-07-27 14:36:25 +0530

    Add files via upload

---
+++ contracts/Lottery.sol
@@ -0,0 +1,38 @@
+ pragma solidity ^0.4.17;
+ 
+ //Contract creation file
+ contract Lottery{
+     
+     address public manager;
+     address[] public players;
+     
+     constructor() public{
+         manager = msg.sender;
+     }
+     
+     function enter() public payable{
+         
+         require(msg.value > 0.1 ether);
+         players.push(msg.sender);
+     }
+     
+     function random() private view returns(uint){
+         return uint(keccak256(abi.encodePacked(block.difficulty, now, players)));
+     }
+     
+     function pickWinner() public restricted{
+         
+         uint index = random() % players.length;
+         players[index].transfer(address(this).balance);
+         players = new address[](0);
+     }
+     
+     modifier restricted(){
+         require(msg.sender == manager);
+         _;
+     }
+     
+     function getPlayers() public view returns(address[]){
+         return players;
+     } 
+ }
